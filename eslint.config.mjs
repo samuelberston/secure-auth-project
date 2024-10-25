c0190a60400e2e94e53bad3465c173f4c65371b4
Samuel Berston <samuelberston@gmail.com>
2024-10-25 12:23:06 -0400

    eslint - configured security plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import pluginSecurity from 'eslint-plugin-security';
 
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  pluginSecurity.configs.recommended
 ];
