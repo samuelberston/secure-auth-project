5723af58c38b2163bd800629097d9eba4be29271
Samuel Berston <samuelberston@gmail.com>
2024-10-25 23:33:20 -0400

    seed schema.sql on start

--- server/psql.js
+++ server/psql.js
@@ -1,5 +1,7 @@
 // Connect to database
+const fs = require('fs')
 const { Pool } = require('pg');
+
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -8,5 +10,13 @@ const pool = new Pool({
   password: '',
   port: 5432
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  const seedQuery = fs.readFileSync('database/schema.sql', { encoding: 'utf8' })
+  pool.query(seedQuery, (err, res) => {
+      // console.log(err, res)
+      console.log('Seeding Completed!')
+  })
+}
 
 module.exports = pool;
