f4bec883b4bd8487e43a240475074fba4b57a8d0
Samuel Berston <samuelberston@gmail.com>
2024-10-25 23:32:48 -0400

    schema.sql - fix issues

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS auth_db;
+/* DROP DATABASE IF EXISTS auth_db;
 
 CREATE DATABASE auth_db;
 
-CREATE SCHEMA auth_db;
+CREATE SCHEMA auth_db;  */
 
 CREATE TABLE users (
     user_uuid UUID PRIMARY KEY,
     username VARCHAR(30) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    salt TEXT NOT_NULL.,
+    salt TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 )
