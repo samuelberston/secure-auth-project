7f150dbcb5904348a19526efd353b46f2f9a32e7
Samuel Berston <samuelberston@gmail.com>
2024-11-01 15:05:45 -0400

    remove unused getCookie function - removed csrf token

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,10 +1,3 @@
-// Function to get a cookie by name
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
 // handle registration
 async function handleRegistration(event) {
     event.preventDefault();
