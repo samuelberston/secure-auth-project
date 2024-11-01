c9f3efed4077abdeb1c75288f31e662ae9c6dd86
Samuel Berston <samuelberston@gmail.com>
2024-11-01 16:43:50 -0400

    jest.config.js - convert file to ES module

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,28 @@
 // jest.config.js
-module.exports = {
-    projects: [
-      {
-        displayName: "backend",
-        testEnvironment: "node",
-        testMatch: ["<rootDir>/server/tests/**/*.test.js"],
-        transform: {
-          "^.+\\.js$": "babel-jest"
-        },
-        moduleFileExtensions: ["js"]
-      },
-      {
-        displayName: "frontend",
-        testEnvironment: "jsdom",
-        testMatch: ["<rootDir>/frontend/tests/**/*.test.js"],
-        transform: {
-          "^.+\\.js$": "babel-jest"
-        },
-        moduleFileExtensions: ["js", "mjs"],
-        globals: {
-          "babel-jest": {
-            useESM: true
-          }
-        },
-        setupFilesAfterEnv: ["<rootDir>/jest.setup.js"] // Add this line
+export const projects = [
+  {
+    displayName: "backend",
+    testEnvironment: "node",
+    testMatch: ["<rootDir>/server/tests/**/*.test.js"],
+    transform: {
+      "^.+\\.js$": "babel-jest"
+    },
+    moduleFileExtensions: ["js"]
+  },
+  {
+    displayName: "frontend",
+    testEnvironment: "jsdom",
+    testMatch: ["<rootDir>/frontend/tests/**/*.test.js"],
+    transform: {
+      "^.+\\.js$": "babel-jest"
+    },
+    moduleFileExtensions: ["js", "mjs"],
+    globals: {
+      "babel-jest": {
+        useESM: true
       }
-    ]
-  };
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+  }
+];
   
