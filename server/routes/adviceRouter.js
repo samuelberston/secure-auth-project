0eab80a12746fe737e490d6c5fc7e1b02294fa20
Samuel Berston <samuelberston@gmail.com>
2024-11-10 00:03:21 -0600

    logging for adviceRouter and protectedRouter

--- server/routes/adviceRouter.js
+++ server/routes/adviceRouter.js
@@ -3,7 +3,7 @@ const AdviceRouter = require("express").Router();
 const rateLimit = require('express-rate-limit');
 const NodeCache = require('node-cache');
 
-// PostgreSQL connection
+const logger = require('../logger.js');
 const pool = require('../psql.js');
 
 // Rate limiting middleware
@@ -33,18 +33,21 @@ AdviceRouter.get('/', async (req, res) => {
 
         // Return cached advice if available
         if (adviceCache.get(dayOfWeek)) {
+            logger.info(`Sent cached advice to IP: %s`, req.ip);
             return res.status(200).json({ advice: adviceCache.get(dayOfWeek) });
         }
         const advice = await pool.query('SELECT advice FROM advice WHERE day_of_week = $1', [dayOfWeek]);
         if (advice.rows.length === 0) {
+            logger.error(`Advice was not found from the database`);
             return res.status(404).json({ message: "No advice found for today" });
         }
         // Cache the advice
         adviceCache.set(dayOfWeek, advice.rows[0].advice) = dayOfWeek;
-
+        logger.info(`Sent advice to IP: %s`, req.ip);
         return res.status(200).json(advice.rows[0]);
     } catch (err) {
         console.error("Error fetching advice: ", err);
+        logger.error(`Error while sending advice to IP: %s. Error: %s`, req.ip, err);
         return res.status(500).json({ message: "Internal server error" });
     }
 });
--- server/routes/protectedRouter.js
+++ server/routes/protectedRouter.js
@@ -3,6 +3,8 @@
  */
 
 const jwt = require('jsonwebtoken');
+const logger = require('../logger.js');
+
 const ProtectedRouter = require('express').Router();
 
 // Middleware to authenticate token
@@ -10,16 +12,23 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Extract token after 'Bearer'
 
-    if (!token) return res.status(401).json({ message: 'Token missing.' });
+    if (!token) {
+        logger.warn(`Unauthorized attempt to access protected data from IP: %s`, req.ip);
+        return res.status(401).json({ message: 'Token missing.' });
+    }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Token invalid or expired.' });
+        if (err) {
+            logger.warn(`Attempt to access protected data with expired JWT from IP: %s`, req.ip);
+            return res.status(403).json({ message: 'Token invalid or expired.' });
+        }
         req.user = user;
         next();
     });
 }
 
 ProtectedRouter.get('/', authenticateToken, (req, res) => {
+    logger.info(`User %s accessed protected data from IP: %s`, req.user, req.ip);
     res.status(200).json({ data: 'This is protected data.', user: req.user });
 });
 
