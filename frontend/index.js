bcaa3453a8b2e6666bbe6b744257dc7e8c386eaa
Samuel Berston <samuelberston@gmail.com>
2024-11-15 16:00:09 -0800

    frontend/index.js - use BACKEND_URL referring to docker container

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const { createProxyMiddleware }= require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+const PORT = 80;
+const BACKEND_URL = process.env.BACKEND_URL || 'http://backend:3000'; 
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Proxy configuration: forward API requests to the backend server
 const proxyMiddleware = createProxyMiddleware({
-    target: 'http://localhost:3000', // backend server
+    target: BACKEND_URL, // backend server
     changeOrigin: true,
     pathRewrite: {
         '^/api': '', // Remove '/api' prefix when forwarding
@@ -23,12 +26,11 @@ const proxyMiddleware = createProxyMiddleware({
 
 app.use('/api', proxyMiddleware);
 
-// Route to serve the registration form
+// Serve the html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = 80;
 app.listen(PORT, () => {
     console.log(`Client server running on http://localhost:${PORT}`);
 });
