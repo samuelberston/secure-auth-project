e8a707738c0e5dd9de6fd05761cf209d4c929ead
Samuel Berston <samuelberston@gmail.com>
2024-11-15 17:09:48 -0800

    psql - update connection for docker development environment

--- backend/psql.js
+++ backend/psql.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DD_URL,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER || "samuelberston",
+  host: process.env.DD_URL || 'host.docker.internal',
+  database: process.env.DB_NAME || "auth_db",
   password: '',
   port: 5432
 });
