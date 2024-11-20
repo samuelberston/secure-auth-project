97dbb61b9885c402b512f91b47954718d7dafb71
Samuel Berston <samuelberston@gmail.com>
2024-11-19 22:00:38 -0800

    vpc.tf - update azs to b and c

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "secure-auth-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region}a", "${var.region}b"]
+  azs             = ["${var.region}b", "${var.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
