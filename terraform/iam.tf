1fdef98b79cd5cebf1a6df3cc6f8aefb2d654314
Samuel Berston <samuelberston@gmail.com>
2024-11-23 16:52:13 -0800

    terraform/iam.tf - update policy for EKS cluster creation

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,64 @@
-// IAM policies
+# IAM policy for EKS cluster creation
+resource "aws_iam_policy" "eks_cluster_creation" {
+  name        = "eks-cluster-creation-policy"
+  description = "Policy for creating EKS cluster and related resources"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:CreateKey",
+          "kms:TagResource",
+          "kms:CreateAlias",
+          "kms:DeleteAlias",
+          "kms:UpdateAlias",
+          "kms:GetKeyPolicy",
+          "kms:PutKeyPolicy",
+          "logs:CreateLogGroup",
+          "logs:DeleteLogGroup",
+          "logs:PutRetentionPolicy",
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:PutRolePolicy",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:CreateServiceLinkedRole",
+          "iam:ListRoles",
+          "iam:ListInstanceProfiles",
+          "iam:PassRole", 
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeRouteTables",
+          "ec2:DescribeInternetGateways",
+          "ec2:DescribeNatGateways",
+          "ec2:CreateSecurityGroup",
+          "ec2:CreateTags",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:CreateFlowLogs",
+          "ec2:DeleteFlowLogs",
+          "eks:DescribeCluster",
+          "eks:CreateCluster",
+          "eks:DeleteCluster",
+          "eks:UpdateClusterVersion",
+          "eks:UpdateClusterConfig",
+          "eks:ListClusters",
+          "eks:TagResource",
+          "eks:UntagResource"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to the user
+resource "aws_iam_user_policy_attachment" "eks_cluster_creation" {
+  user       = "secure-auth-project"
+  policy_arn = aws_iam_policy.eks_cluster_creation.arn
+}
