1d9e0b84edc68926c5760585658d101f47e7960b
Samuel Berston <samuelberston@gmail.com>
2024-11-26 20:50:39 -0800

    vpc - endpoints

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,16 +9,67 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
   
-  # Add IAM role for VPC flow logs
+  # IAM role for VPC flow logs
   enable_flow_log                           = true
   create_flow_log_cloudwatch_iam_role       = true
   create_flow_log_cloudwatch_log_group      = true
   flow_log_destination_type                 = "cloud-watch-logs"
   flow_log_cloudwatch_log_group_name_prefix = "/aws/vpc-flow-logs/"
+  
+  # Required endpoints for EKS
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+  tags = {
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
+}
+
+module "vpc_endpoints" {
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+
+  endpoints = {
+    s3 = {
+      service = "s3"
+      tags    = { Name = "s3-vpc-endpoint" }
+    }
+    ec2 = {
+      service             = "ec2"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+    sts = {
+      service             = "sts"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+    autoscaling = {
+      service             = "autoscaling"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+    logs = {
+      service             = "logs"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    }
+  }
 
   tags = {
     Environment = "production"
